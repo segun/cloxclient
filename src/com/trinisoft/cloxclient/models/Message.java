eaaa868fb1dc1f9c8d270b8a3b28385c4b2a31da
segun <akintayo.segun@gmail.com>
2010-03-09 14:02:48 +0100

    We can not line-wrap the recieved and sent messages in the list

--- src/com/trinisoft/cloxclient/models/Message.java
+++ src/com/trinisoft/cloxclient/models/Message.java
@@ -48,14 +48,7 @@ public class Message {
     public void setTo(String to) {
         this.to = to;
     }
-
-    @Override
-    public String toString() {
-        return "<i>" + this.getTime() + "</i> <b style='color:green'>from " + this.getFrom() + "</b> " +
-                "<b style='color:red'>to " + this.getTo() + "</b><br />" +
-                "<div style='margin-left:25px'>" + this.getMsg() + "</div><br />";
-    }
-
+    
     public String toNamedString() {
         return "<a name='curpos'><i>" + this.getTime() + "</i> <b style='color:green'>from " + this.getFrom() + "</b> " +
                 "<b style='color:red'>to " + this.getTo() + "</b><br />" +
--- src/com/trinisoft/cloxclient/ui/HTMLListModel.java
+++ src/com/trinisoft/cloxclient/ui/HTMLListModel.java
@@ -2,12 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.trinisoft.cloxclient.ui;
 
 import com.trinisoft.cloxclient.models.Message;
 import java.util.ArrayList;
-import javax.swing.JLabel;
+import java.util.StringTokenizer;
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
@@ -16,17 +15,30 @@ import javax.swing.event.ListDataListener;
  * @author trinisoftinc
  */
 public class HTMLListModel implements ListModel {
+
     ArrayList<Message> data;
+
     public HTMLListModel(ArrayList<Message> data) {
         this.data = data;
     }
 
     public void addListDataListener(ListDataListener l) {
-        
     }
 
-    public Object getElementAt(int index) {
-        return "<html>" + data.get(index).toString() + "<html>";
+    public Object getElementAt(int index) {        
+        String originalMessage = data.get(index).getMsg();
+        String toReturn = "";        
+        if (originalMessage.length() > 50) {
+            originalMessage = lineBreaks(originalMessage);
+        }
+        System.out.println(originalMessage);
+        toReturn += originalMessage;
+        Message oneMessage = data.get(index);
+        String myData = "<i>" + oneMessage.getTime() + "</i> <b style='color:green'>from " + oneMessage.getFrom() + "</b> " +
+                "<b style='color:red'>to " + oneMessage.getTo() + "</b><br />" +
+                "<div style='margin-left:25px'>" + toReturn + "</div><br />";
+        System.out.println(myData);
+        return "<html>" + myData + "</html>";
     }
 
     public int getSize() {
@@ -34,7 +46,21 @@ public class HTMLListModel implements ListModel {
     }
 
     public void removeListDataListener(ListDataListener l) {
-        
     }
 
+    private String lineBreaks(String msg) {
+        String retVal = "";
+        String prev = "";
+        StringTokenizer tokenizer = new StringTokenizer(msg, " ", true);
+        while(tokenizer.hasMoreTokens()) {            
+            String thisToken = tokenizer.nextToken();
+            if((prev + thisToken).length() > 55) {
+                retVal += "<br>";
+                prev = "";
+            }
+            retVal += thisToken;
+            prev += thisToken;
+        }
+        return retVal;
+    }
 }
